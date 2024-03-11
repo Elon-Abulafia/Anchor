ee427acc396b1e48b19387c6ea1f24474b7a37c4
Elon-Abulafia <elon945@gmail.com>
2024-03-11 16:19:31 +0200

    Add possibility to set cell values

--- spreadsheet_app/utils.py
+++ spreadsheet_app/utils.py
@@ -1,4 +1,6 @@
-from spreadsheet_app.consts import VALID_TYPES
+import os
+import json
+from spreadsheet_app.consts import VALID_TYPES, CONFIG_DIR, TypeTranslator
 
 
 def check_schema_integrity(json_schema):
@@ -27,3 +29,15 @@ def check_schema_integrity(json_schema):
                 break
 
     return schema_is_valid, error_message
+
+
+def validate_new_value_type(sheet_id, column_name, value):
+    with open(f"{os.path.join(CONFIG_DIR, sheet_id)}.json", 'r') as f:
+        config_schema = json.load(f)
+
+        for col in config_schema['columns']:
+            if col["name"] == column_name:
+                if not isinstance(value, TypeTranslator[col["type"]].value):
+                    return False
+
+                return True
