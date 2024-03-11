91c1eb2e171cc44f3552f00e39e0c6e1dc95525e
Elon-Abulafia <elon945@gmail.com>
2024-03-11 12:34:16 +0200

    Add new path to create a new spreadsheet and create run file for the base of the server

--- spreadsheet_app/views.py
+++ spreadsheet_app/views.py
@@ -1,11 +1,22 @@
-from app import app, request, jsonify
+from spreadsheet_app.spreadsheet import create_new_sheet
+from spreadsheet_app import app
+from flask import request, jsonify
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-#
-# @app.route('/sheet', methods=['POST'])
-# def create_sheet():
-#     new_sheet_json = request.json()
+
+@app.route('/sheet', methods=['POST'])
+def create_sheet():
+    new_sheet_json = request.get_json()
+
+    try:
+        sheet_id = create_new_sheet(new_sheet_json)
+    except ValueError as e:
+        response = (f"Invalid json schema: {e}", 400)
+    else:
+        response = (sheet_id, 200)
+
+    return response
